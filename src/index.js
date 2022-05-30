7a3124daa2f43f24c4672e3a1db91a26867618da
Ainsley <ainsley@kloveandsitch.com>
2022-05-30 14:17:28 -0400

    Clean Up File System Jira Issue ID(s):
    	-

--- src/index.js
+++ src/index.js
@@ -2,33 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store";
-// import { persistStore } from "redux-persist";
-// import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PetDetails from "./pages/PetDetails";
-
-// let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={<div>Loading...</div>} persistor={persistor}> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/details" element={<PetDetails />} />
         </Routes>
       </BrowserRouter>
-      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/services/pets.js
+++ src/services/pets.js
@@ -3,17 +3,13 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const petsApi = createApi({
   reducerPath: "pets",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://coolpets.free.beeceptor.com",
+    baseUrl: "https://61f6e0072e1d7e0017fd6f82.mockapi.io",
   }),
   endpoints: (builder) => ({
     getPets: builder.query({
-      query: () => `pets`,
+      query: () => `api/v1/pets`,
     }),
   }),
 });
 
 export const { useGetPetsQuery } = petsApi;
-
-//"https://61f6e0072e1d7e0017fd6f82.mockapi.io"
-
-//api/v1/pets
